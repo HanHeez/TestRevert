14bc7001ff072907a56601bcffa6c204ad08353c
HanHeez <39714812+HanHeez@users.noreply.github.com>
2018-10-07 03:09:46 +0700

    commit 2

--- app/src/main/java/com/gtv/hanhee/testrevert/MainActivity.java
+++ app/src/main/java/com/gtv/hanhee/testrevert/MainActivity.java
@@ -2,16 +2,26 @@ package com.gtv.hanhee.testrevert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtView = findViewById(R.id.txtHello);
-        txtView.setText("ABC");
+        button = findViewById(R.id.btnButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtView.setText("ABC");
+            }
+        });
+
     }
 }
