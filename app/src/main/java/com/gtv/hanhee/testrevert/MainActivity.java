9ea9b0df4d0a47ab9c1b666651cf9e38f64dd8a7
HanHeez <39714812+HanHeez@users.noreply.github.com>
2018-10-07 03:06:52 +0700

    Initial commit

---
+++ app/src/main/java/com/gtv/hanhee/testrevert/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gtv.hanhee.testrevert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
